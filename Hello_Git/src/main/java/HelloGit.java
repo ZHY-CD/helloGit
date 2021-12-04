53568449100e97863985feceba11e23fdfc3577c
ZHY-CD <1170327086@qq.com>
2021-12-04 10:15:56 +0800

    添加分支

--- Hello_Git/src/main/java/HelloGit.java
+++ Hello_Git/src/main/java/HelloGit.java
@@ -3,5 +3,6 @@ public class HelloGit {
         System.out.println("hello git!");
         System.out.println("更新1");
         System.out.println("添加分支");
+        System.out.println("主干在6行添加");
     }
 }
