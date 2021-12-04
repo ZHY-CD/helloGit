b6402af10ce994a734951786908c4a6f1bf8641f
ZHY-CD <1170327086@qq.com>
2021-12-04 10:58:32 +0800

    更改信息

--- Hello_Git/src/main/java/HelloGit.java
+++ Hello_Git/src/main/java/HelloGit.java
@@ -5,5 +5,7 @@ public class HelloGit {
         System.out.println("添加分支");
         System.out.println("主干在6行添加");
         System.out.println("分支在6行添加");
+
+        System.out.println("冲突解决");
     }
 }
